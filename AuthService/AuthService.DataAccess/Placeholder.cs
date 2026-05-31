7f14f622ea8caeead3b44134ab3ffa5fae411bc9
Dmitriy Dankov <apizlon@mail.ru>
2026-05-31 15:00:15 +0300

    Clean up

--- AuthService/AuthService.DataAccess/Placeholder.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthService.DataAccess;
-
-// Этот проект оставлен для сохранения структуры как в примере.
-// В текущей версии AuthService не использует собственную БД.
-public static class Placeholder
-{
-}
-
